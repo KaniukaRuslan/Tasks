a134a2cc93f20f7a8790d0708618698cc2b2d5eb
KaniukaRuslan <71809810+KaniukaRuslan@users.noreply.github.com>
2020-11-11 13:50:54 +0200

    Update Task_1.cs

--- Task_1.cs
+++ Task_1.cs
@@ -6,6 +6,8 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Привіт ,Донну!");
         }
     }
